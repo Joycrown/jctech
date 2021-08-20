db389bd32c73bb608061f90f664df20101a4de9e
olaniyan ayoade <olaniyanayoade@ymail.com>
2021-08-20 02:43:34 +0100

    first commit

---
+++ jctech/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    if request.method =="POST":
+            name = request.POST['name']
+            email = request.POST['email']
+            subject = request.POST['subject']
+            message = request.POST['message']
+
+    else:
+        return render(request, 'home.html', {})
+
+
+
+   
+   
+
+def contact(request):
+    if request.method =="POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+
+        return render(request, 'home.html')
+
+
+
+    else:
+        return render(request, 'home.html', {})
---
+++ name.py
@@ -0,0 +1,5 @@
+def print_name():
+    print("My is: \nOlaniyan Ayoade")
+
+if __name__ =='_main_':
+    print_name()
